d56c9624a17d053a84a8c48f3108932d0849e3c2
YokerHS <hiimyoker@gmail.com>
2025-05-19 12:07:00 +0900

    first commit

---
+++ src/main/java/com/example/git_sample_yoker/GitSampleYokerApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_sample_yoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleYokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleYokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/git_sample_yoker/GitSampleYokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_sample_yoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSampleYokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
